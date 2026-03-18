8380a58c159df5a816acb640bde2f4d0ce976bc0
Igack <legoae86@gmail.com>
2026-03-18 11:41:11 -0400

    finish run scheudler

--- scheduler.c
+++ scheduler.c
@@ -86,32 +86,62 @@ void terminate_process(PCB *pcb, int tick) { state_transition(pcb, RUNNING, TERM
 
 void run_scheduler(Scheduler *s, PCB **processes, int size, int quantum) {
     s->quantum = quantum;
+    int running_scheduler = 1;
 
-    for (int i = 0; i < size; i++) {
-        if (processes[i]->arrival_time == s->tick && processes[i]->state == NEW) { enqueue(s, processes[i]); }
-    }
-
-    if (s->running != NULL) {
-        s->running->remaining_burst--;
-        s->running->program_counter++;
-        s->running->cpu_time_used++;
-        s->ticks_on_cpu++;
-        if (s->running->remaining_burst == 0) {
-            terminate_process(s->running, quantum);
-            s->running->finish_time = s->tick;
-            s->running = NULL;
-            s->ticks_on_cpu = 0;
-        } else if (s->ticks_on_cpu >= s->quantum){
-            preempt(s->running, s->tick);
-            enqueue(s, s->running);
-            s->running = NULL;
-            s->ticks_on_cpu = 0;
+    while (running_scheduler) {
+         for (int i = 0; i < size; i++) {
+            if (processes[i]->state == WAITING) {
+                processes[i]->io_time_remaining--;
+                if (processes[i]->io_time_remaining == 0) {
+                    io_complete(processes[i], s->tick);
+                    enqueue(s, processes[i]);
+                }
+            } else if (processes[i]->arrival_time == s->tick && processes[i]->state == NEW) {
+                admit_process(processes[i], s->tick);
+                enqueue(s, processes[i]);
+            }
         }
 
-        return;
-    } else if (s->running == NULL && is_empty(s) == 0) {
+        if (s->running != NULL) {
+            s->running->remaining_burst--;
+            s->running->program_counter++;
+            s->running->cpu_time_used++;
+            s->ticks_on_cpu++;
+            if (s->running->remaining_burst <= 0) {
+                if (s->running->io_burst_length > 0) {
+                    s->running->io_time_remaining = s->running->io_burst_length;
+                    s->running->io_burst_length = 0;
+                    block_on_io(s->running, s->tick);
+                    s->running = NULL;
+                    s->ticks_on_cpu = 0;
+                } else {
+                    terminate_process(s->running, s->tick);
+                    s->running->finish_time = s->tick;
+                    s->running = NULL;
+                    s->ticks_on_cpu = 0;
+                }
+            } else if (s->ticks_on_cpu >= s->quantum){
+                preempt(s->running, s->tick);
+                enqueue(s, s->running);
+                s->running = NULL;
+                s->ticks_on_cpu = 0;
+            }
 
+        } else if (s->running == NULL && is_empty(s) == 0) {
+            s->running = dequeue(s);
+            dispatch(s->running, s->tick);
+
+        } else if (s->running == NULL && is_empty(s) == 1) {
+            int complete_flag = 1;
+            for (int i = 0; i < size; i++) {
+                if (processes[i]->state != TERMINATED) {
+                    complete_flag = 0;
+                    break;
+                }
+            }
+            if (complete_flag) running_scheduler = 0;
+        }
+
+        s->tick++;
     }
-
-
 }
